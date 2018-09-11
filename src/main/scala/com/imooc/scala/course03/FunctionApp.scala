bec83056874128050a961c12ba9aade8bc6a65a1
lingh <linguanghuan@gmail.com>
2018-09-11 23:50:14 +0800

    scala函数

--- src/main/scala/com/imooc/scala/course03/FunctionApp.scala
+++ src/main/scala/com/imooc/scala/course03/FunctionApp.scala
@@ -12,6 +12,32 @@ object FunctionApp {
     sayHello()
     sayHello
     sayHello("zhangsan")
+    sayName()  // 这里括号不能省略
+    sayName("ruoze")
+
+    // 命名参数
+    println(speed(100, 10))
+    println(speed(time = 10,distance = 100))
+
+    // 可变参数 *
+    println(sum(1,2,3,4,5,6,7,8,9))
+
+    // 循环表达式
+    for( i<- 1 to 10 if i%2==0) {
+      println(i)
+    }
+    val courses = Array("Hadoop", "Spark SQL", "Spark Streaming")
+    for(course<-courses) {
+      println(course)
+    }
+    // => 将左边的course作用上一个函数变成另外一个结果
+    courses.foreach(course=>println(course))
+    var (num, sumResult) = (100, 0)
+    while(num > 0) {
+      sumResult = sumResult + num
+      num = num -1
+    }
+    println(sumResult)
   }
 
   def add(x:Int, y: Int): Int = {
@@ -26,5 +52,24 @@ object FunctionApp {
 
   def sayHello(name:String)={
     println(" hello " + name)
+  }
+
+  // 默认参数
+  def sayName(name:String="PK") = {
+    println(name)
+  }
+
+  // 命名参数
+  def speed(distance:Float, time:Float):Float =  {
+    distance / time
+  }
+
+  // 可变参数
+  def sum(numbers:Int*):Int = {
+    var result = 0
+    for(number<-numbers) {
+      result += number
+    }
+    result
   }
 }
